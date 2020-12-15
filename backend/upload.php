07eb90bca022c3afdc0c2e15821ba09c89fe5375
Ahmed Mansoor <ahmadh.mansoor@gmail.com>
2020-12-15 19:23:45 +0500

    add car w/o image

--- backend/upload.php
+++ backend/upload.php
@@ -1,25 +1,23 @@
 <?php
 
-
-
+// Create database connection
 include_once("../db/pdoconn.php");
-// $carname = $price = $carimg = $fromdate = $todate = '';
-
 
 if (isset($_POST['btnUpload'])) {
     echo '<pre>';
     var_dump($_POST);
     echo '<pre>';
 
+    // return $cars[$key] ?? null;
 
-    $sql = "INSERT INTO cars (carname, price, fromdate, todate) 
+    $sql = "INSERT INTO cars (carname, price,  fromdate, todate) 
                 VALUES(:carname,:price, :fromdate, :todate)";
 
     $stmt = $pdo->prepare($sql);
     $stmt->execute([
         'carname' => $_POST['carname'],
         'price' => $_POST['price'],
-        // 'carimg' => $_POST['carimg'],
+        // 'image' => $_POST['image'],
         'fromdate' => $_POST['fromdate'],
         'todate' => $_POST['todate'],
     ]);
--- db/pdoconn.php
+++ db/pdoconn.php
@@ -3,4 +3,10 @@
 $dbServername = "root";
 $dbPassword = "";
 
-$pdo = new PDO('mysql:host=localhost;dbname=cardb', $dbServername, $dbPassword);
+try {
+
+    $pdo = new PDO('mysql:host=localhost;dbname=cardb', $dbServername, $dbPassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
